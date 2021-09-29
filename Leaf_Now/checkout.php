b70502d3f6844b3da8ad09e6232a0b6f1e7f1a21
sandeshr07 <sandeshputtu7gowda@gmail.com>
2021-09-29 22:33:39 +0530

    new

---
+++ Leaf_Now/checkout.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("connection/connect.php");
+include_once 'product-action.php';
+error_reporting(0);
+session_start();
+if(empty($_SESSION["user_id"]))
+{
+	header('location:login.php');
+}
+else{
+
+
+												foreach ($_SESSION["cart_item"] as $item)
+												{
+
+												$item_total += ($item["price"]*$item["quantity"]);
+
+													if($_POST['submit'])
+													{
+
+													$SQL="insert into users_orders(u_id,title,quantity,price) values('".$_SESSION["user_id"]."','".$item["title"]."','".$item["quantity"]."','".$item["price"]."')";
+
+														mysqli_query($db,$SQL);
+
+														$success = "Thankyou! Your Order Placed successfully!";
+
+
+														header('Location: index.php');
+													}
+												}
+?>
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="#">
+    <title>LeafNow</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet"> </head>
+<body>
+
+    <div class="site-wrapper">
+        <!--header starts-->
+        <header id="header" class="header-scroll top-header headrom">
+            <!-- .navbar -->
+            <nav class="navbar navbar-dark">
+                <div class="container">
+                    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                    <a class="navbar-brand" href="index.html">LeafNow</a>
+                    <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="stores.php">Stores<span class="sr-only"></span></a> </li>
+
+							<?php
+						if(empty($_SESSION["user_id"]))
+							{
+								echo '<li class="nav-item"><a href="login.php" class="nav-link active">login</a> </li>
+							  <li class="nav-item"><a href="registration.php" class="nav-link active">signup</a> </li>';
+							}
+						else
+							{
+
+
+										echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">your orders</a> </li>';
+									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">logout</a> </li>';
+							}
+
+						?>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- /.navbar -->
+        </header>
+        <div class="page-wrapper">
+
+
+                <div class="container">
+
+					   <span style="color:green;">
+								<?php echo $success; ?>
+										</span>
+
+                </div>
+
+
+
+
+            <div class="container m-t-30">
+			<form action="" method="post">
+                <div class="widget clearfix">
+
+                    <div class="widget-body">
+                        <form method="post" action="#">
+                            <div class="row">
+
+                                <div class="col-sm-12">
+                                    <div class="cart-totals margin-b-20">
+                                        <div class="cart-totals-title">
+                                            <h4>Cart Summary</h4> </div>
+                                        <div class="cart-totals-fields" id="divToPrint">
+
+                                            <table class="table">
+											<tbody>
+
+
+
+                                                    <tr>
+                                                        <td>Cart Subtotal</td>
+                                                        <td> <?php echo "₹".$item_total; ?></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Shipping &amp; Handling</td>
+                                                        <td>free shipping</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="text-color"><strong>Total</strong></td>
+                                                        <td class="text-color"><strong> <?php echo "₹".$item_total; ?></strong></td>
+                                                    </tr>
+                                                </tbody>
+
+
+
+
+                                            </table>
+                                        </div>
+                                    </div>
+                                    <!--cart summary-->
+                                    <div class="payment-option">
+                                        <ul class=" list-unstyled">
+                                            <li>
+                                                <label class="custom-control custom-radio  m-b-20">
+                                                    <input name="m" id="radioStacked1" checked value="COD" type="radio" class="custom-control-input"> <span class="custom-control-indicator"></span> <span class="custom-control-description">Payment on delivery</span>
+                                                    <br> <span>Please send your cheque to Store Name, Store Street, Store Town, Store State / County, Store Postcode.</span> </label>
+                                            </li>
+                                            <li>
+                                                <label class="custom-control custom-radio  m-b-10">
+                                                    <input name="m" type="radio" value="paypal" disabled class="custom-control-input"> <span class="custom-control-indicator"></span> <span class="custom-control-description">Paypal <img src="images/paypal.jpg" alt="" width="90"></span> </label>
+                                            </li>
+                                        </ul>
+                                         <input type="submit" onclick="return confirm('Are you sure?');" name="submit"  class="btn btn-outline-success btn-block" value="Order now">
+                                         <button onclick="PrintDiv();">Print Receipt</button>
+                                    </div>
+									</form>
+                                </div>
+                            </div>
+
+                    </div>
+                </div>
+				 </form>
+            </div>
+
+            <!-- start: FOOTER -->
+
+            <!-- end:Footer -->
+        </div>
+        <!-- end:page wrapper -->
+         </div>
+
+    <script>
+        function PrintDiv() {
+       var divToPrint = document.getElementById('divToPrint');
+       var popupWin = window.open('', '_blank', 'width=800,height=800');
+       popupWin.document.open();
+       popupWin.document.write('<html><body onload="window.print()">' + divToPrint.innerHTML + '</html>');
+        popupWin.document.close();
+            }
+    </script>
+     <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+</body>
+
+</html>
+
+<?php
+}
+?>
